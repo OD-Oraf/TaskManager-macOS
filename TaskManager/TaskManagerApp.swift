30377f1f597dce7c45c3bbb883eb99deaf0b108d
odoraf <poseidonog@gmail.com>
2024-03-15 21:56:59 -0400

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by OD Orafidiya on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
